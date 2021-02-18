bcc210c8d10f0e72df4a45cd813a670c9f007363
hemachandran <hemavikic@gmail.com>
2021-02-18 12:51:18 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-// redfunction() {
-//     documen
-// }
+function blue()
+{
+    document.getElementById("image-resize").src="images/seltos_blue.webp";
+    document.getElementById("textp").innerHTML="Paint:Blue";
+}
+
+function black()
+{
+    document.getElementById("image-resize").src="images/seltos_black.webp";
+    document.getElementById("textp").innerHTML="Paint:Black";
+
+}
+
+function orange()
+{
+    document.getElementById("image-resize").src="images/seltos_orange.jpg";
+    document.getElementById("textp").innerHTML="Paint:Orange";
+
+}
